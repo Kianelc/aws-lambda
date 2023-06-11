2522693034fe44a38ac70b197eab15ab115bf640
Kiane L. Casagrande <kiane.casagrande@gmail.com>
2023-06-11 20:41:51 -0300

    Buscando objetos do S3

--- index.mjs
+++ index.mjs
@@ -1,23 +1,26 @@
 import { log } from "./log.mjs";
+import { S3 } from "@aws-sdk/client-s3"
 
+const s3Client = new S3({ region: 'us-east-1' })
 export const handler = async(event) => {
-    // TODO implement
-    const event_body = JSON.stringify(event);
 
-    log(`event: ${event_body}`);
+    const record = event.records[0];
+    const Bucket = record.s3.bucket.name;
+    const Key = record.s3.object.key;
+    const getObjectResult = await s3Client.getObject({
+        Bucket,
+        Key,
+    });
 
-    return {
-        statusCode: 200,
-        body: `
-        <!DOCTYPE html>
-        <html>
-        <body>
-            <h3>Dados da requisição:</h3>
-            <p>${event_body}</p>
-        </body>
-        </html>`,
-        headers: {
-            "content-type": "text/html;charset=UTF-8"
-        }
-    };
+    const mega_byte = 1024 * 1024;
+
+    if(getObjectResult.ContentLength > 1 * mega_byte) {
+        log('Objeto muito grande');
+
+        return 'Objeto muito grande';
+    }
+
+    log('Objeto de tamanho OK');
+
+    return 'Objeto de tamanho OK';
 };
